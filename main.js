69632421e1269db2d18d62c5624f58df4c085b83
Purple4pur <purple4pur@gmail.com>
2020-03-12 23:23:21 +0800

    IMPROVE: set outline for debugging

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 400,
     resizable: false,
     useContentSize: true,
     webPreferences: {
